1fbd82614dd88cf98baa0bb0a4abfa3765f24d60
Hendra Anggrian <hendraanggrian@gmail.com>
2019-10-14 03:13:38 +0700

    Check test sources

--- prefs-android/build.gradle.kts
+++ prefs-android/build.gradle.kts
@@ -28,6 +28,9 @@ android {
             resources.srcDir("tests/src")
         }
     }
+    lintOptions {
+        isCheckTestSources = true
+    }
     libraryVariants.all {
         generateBuildConfigProvider?.configure {
             enabled = false
